3a2d8290127ce48b3987c04a844bf2680c1c58f5
Eoghan Gallagher <eoghan.gallagher@gmail.com>
2018-04-21 18:52:01 +0100

    Bolger

---
+++ public/js/app.js
@@ -0,0 +1,19 @@
+
+//Side Nav
+const sideNav = document.querySelector('.sidenav');
+  M.Sidenav.init(sideNav, {});
+
+
+
+const mbOptions = {
+
+};
+//Gallery
+const mb = document.querySelectorAll('.materialboxed');
+   M.Materialbox.init(mb, {overlayActive : false});
+
+
+//scrollspy
+const ss = document.querySelectorAll('.scrollspy');
+
+M.ScrollSpy.init(ss, {});
